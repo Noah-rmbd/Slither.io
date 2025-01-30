ed3fa0de0ad6ff9486fd87be5c8c9fd92a29290f
Noah-rmbd <157590199+Noah-rmbd@users.noreply.github.com>
2025-01-30 17:22:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from MainGame import MainGame
+from StartMenu import StartMenu
+import pygame
 
-game = MainGame()
+pygame.init()
+winDims = (1000, 700)
+window = pygame.display.set_mode(winDims)
 
+start_menu = StartMenu(window, winDims)
+user_name = start_menu.user_name
+game = MainGame(window, winDims, user_name)
 game.init()
 
-# 1) create a student variable
-
-# 2) print out his name
-print()
+# Loop to restart the game when you loose
+while (game.quit and game.refresh) or game.home:
+    if game.home:
+        start_menu = StartMenu(window, winDims)
+        user_name = start_menu.user_name
+    game = MainGame(window, winDims, user_name)
+    game.init()
