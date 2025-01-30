785d7f811fd525ebefb2978b7da37795ec60f18f
Noah-rmbd <157590199+Noah-rmbd@users.noreply.github.com>
2025-01-30 17:24:39 +0100

    Update FontRenderer.py

--- FontRenderer.py
+++ FontRenderer.py
@@ -3,10 +3,10 @@ import pygame
 class FontRenderer:
     def __init__(self):
         self.color = (255,255,0)
-        self.size = 80
-        self.font = pygame.font.Font("fonts/Minecraft.ttf",self.size)
+        self.size = 30
+        self.font = pygame.font.Font("DIN_bold.ttf",self.size)
 
 
-    def renderFont(self,window,score):
-        text = self.font.render("Score: " + str(score),True,self.color)
+    def renderFont(self,window,score,texte):
+        text = self.font.render("Score: " + str(score) + texte,True,self.color)
         window.blit(text,(0,0))
